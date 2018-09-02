96b9fd5e0ba1bb6d36965979e119669bc6613a06
__Irvan_Mac_Pro__ <irvanrefnaldy98@gmail.com>
2018-09-02 13:30:38 +0700

    add validate

---
+++ helpers/routeHelpers.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+
+module.exports = {
+  validateParam : (schema, name) => {
+    return (req, res, next) => { 
+      const result = Joi.validate({param: req['params'][name]}, schema)
+      if( result.error ){
+        // Error happends
+        return res.status(404).json(result.error);
+      }else{
+        if(!req.value)
+          req.value = {};
+        
+        if(!req.value['params'])
+          req.value['params'] = {};
+        
+        req.value['params'][name] = result.value.param;
+      
+        next()
+      }
+    }
+  },
+
+  schemas : {
+    idSchema : Joi.object().keys({
+      param : Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+    })
+  }
+}
